c89839d006c488b6b1372953fa26743e7ec409f6
Lautte <laura.c.hurd@gmail.com>
2015-06-06 16:52:16 -0500

    laura's and Dee's program for scraping and adding span tags to bills

--- bill-parse.py
+++ bill-parse.py
@@ -1,14 +1,19 @@
 
-import re
-bill=open('bill.txt', encoding='utf-8')
-#print(bill.read())
-parse_this=bill.read()
 
 
-#q2 = re.compile("^"+"/./")#searches for periods
-#periods = q2.search(parse_this)
+import requests, bs4
 
-period=parse_this.split('.')
+bill_number = input('bill number')
+if not bill_number.isalnum():
+    print('error must be a number')
+url = "http://www.capitol.state.tx.us/tlodocs/84R/billtext/html/SB000" + bill_number + "I.htm" #this suffix changes depending on what stage the bill is at. we could give them an option
+res = requests.get(url)
+if not res.status_code == requests.codes.ok:
+    print('not a vaild bill!')
+html = bs4.BeautifulSoup(res.text)
+clean_text = html.get_text()
+
+period=clean_text.split('.')
 #print (period)
 
 index=len(period)-1
