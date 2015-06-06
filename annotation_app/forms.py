ef8c65d61a3a54cf76f52ab8a5e24396533bead6
Ed Solis <edsfocci@gmail.com>
2015-06-06 18:22:58 -0500

    Add Annotations and Comments

--- annotation_app/forms.py
+++ annotation_app/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from annotation_app.models import Bill, Comment
 
 class BillForm(forms.Form):
+  text = forms.CharField(label='text')
+
+class AnnotationForm(forms.Form):
+  bill_id = forms.HiddenInput()
+  text = forms.CharField(label='text')
+
+class CommentForm(forms.Form):
+  annotation_id = forms.HiddenInput()
   text = forms.CharField(label='text')
