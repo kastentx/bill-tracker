2d4f25c36d27a846355e7756377dd80b690d0528
fdrozdowski <fgd1@rice.edu>
2015-06-06 21:00:36 -0500

    Pulling bills from Texas Legislature Online

--- annotation_app/views.py
+++ annotation_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, redirect
 import requests
 from annotation_app.models import Bill, Annotation, Comment
-from annotation_app.forms import BillForm#, AnnotationForm, CommentForm
+from annotation_app.forms import AnnotationForm, CommentForm, BillForm
 
 def index(request):
   return render(request, 'base.html')
@@ -15,31 +15,44 @@ def add_bill(request):
       data = form.cleaned_data
       bill = Bill()
       bill.number = data['number']
-
-      if not str(bill.number).isalnum():
-        print('error must be a number')
-      # Queries only senate bills legislative session 84R
-      url = "http://www.capitol.state.tx.us/tlodocs/84R/billtext/html/SB000" + str(bill.number) + "I.htm" #this suffix changes depending on what stage the bill is at. we could give them an option
-      print("url = " + url)
-      print(requests)
-      res = requests.get(url)
-      if not res.status_code == requests.codes.ok:
-        print('not a vaild bill!')
-
-      html = bs4.BeautifulSoup(res.text)
-      clean_text = html.get_text()
-      print(clean_text)
-
+      bill_txt = get_bill_text(str(bill.number))
+      if (bill_txt == None):
+        return "Invalid bill number."
+      else:
+        bill.text = bill_txt
       bill.save()
       return HttpResponseRedirect("/index/")
   else:
     form = BillForm()
   return render(request, 'addbill.html', {'form': form})
 
-def get_bill_data(number):
-#TODO working on it
-    bill = {}
-    bill[""]
+
+
+def get_bill_text(number):
+
+  if not number.isalnum():
+    print('error must be a number')
+  # Queries only senate bills in legislative session 84R
+  url = "http://www.capitol.state.tx.us/tlodocs/84R/billtext/html/SB000" + number + "I.htm"
+  #this suffix changes depending on what stage the bill is at. we could give them an option
+
+  res = requests.get(url)
+  if not res.status_code == requests.codes.ok:
+    return None
+
+  html = bs4.BeautifulSoup(res.text)
+  clean_text = html.get_text()
+  # this is actually a list of sentences
+  sentence_list = clean_text.split('.')
+  span_text = ""
+
+  for sentence in sentence_list:
+
+    modified_sentence = sentence.replace('\n',"").replace('\t',"").replace('\xa0',"").replace('\r',"")
+    span = '<span>' + modified_sentence + '</span>'
+    span_text += span
+
+  return span_text
 
 
 def bill(request, bill_id):
